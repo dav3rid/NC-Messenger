c748c6dda173f926f7e2c44fd0db5b9d79d38885
Simon Green <simonisaacgreen@icloud.com>
2019-10-23 15:07:02 +0100

    making layout responsive

--- src/components/MessageList.jsx
+++ src/components/MessageList.jsx
@@ -3,21 +3,37 @@ import styled from 'styled-components';
 import MessageCard from './MessageCard';
 
 const MessageBox = styled.div`
-  padding: 20px 0 0 20px;
   height: 60vh;
-  width: 90%;
   border: 2px solid black;
   border-radius: 5px;
   overflow-y: auto;
   background-color: #ededed;
 `;
 
+const MessageGridWrapper = styled.div`
+  grid-area: main;
+  display: grid;
+  grid-template-columns: minmax(1.2rem, 1fr) minmax(auto, 600px) minmax(
+      1.2rem,
+      1fr
+    );
+`;
+
+const MessageContent = styled.div`
+  grid-column: 2;
+  padding-top: 20px;
+`;
+
 const MessageList = props => {
   return (
     <MessageBox>
-      {props.messages.map((message, index) => {
-        return <MessageCard message={message} key={index} />;
-      })}
+      <MessageGridWrapper>
+        <MessageContent>
+          {props.messages.map((message, index) => {
+            return <MessageCard message={message} key={index} />;
+          })}
+        </MessageContent>
+      </MessageGridWrapper>
     </MessageBox>
   );
 };
